a4f26542e2e2d19c711c7d91b2bfa6b1326cd354
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-30 15:20:30 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,17 +28,43 @@ class Owner
   end
 
   def cats
-    Cat.all.select {|i| i.owner.name == self.name}
+    Cat.all.select {|i| i.owner == self}
   end
 
   def dogs
-    Dog.all.select {|i| i.owner.name == self.name}
+    Dog.all.select {|i| i.owner == self}
   end
 
-  # def buy_cat(name)
-  #   new_cat = Cat.new(name, self)
-  #   Cat.all << new_cat
-  # end
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
 
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+   # binding.pry
+    self.dogs.select {|i| i.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.select {|i| i.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.select do |i| 
+      i.mood = "nervous"
+       i.owner = nil
+    end
+    self.dogs.select do |i| 
+      i.mood = "nervous"
+       i.owner = nil
+    end
+  end
+
+  def list_pets
+    return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 
 end
