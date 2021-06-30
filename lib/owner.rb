d928c92d61641b58ca734f28f3052a28a1eb20b1
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-30 14:42:27 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|i| i.owner.name == self.name}
+  end
+
+  def dogs
+    Dog.all.select {|i| i.owner.name == self.name}
+  end
+
+  # def buy_cat(name)
+  #   new_cat = Cat.new(name, self)
+  #   Cat.all << new_cat
+  # end
+
+
 end
